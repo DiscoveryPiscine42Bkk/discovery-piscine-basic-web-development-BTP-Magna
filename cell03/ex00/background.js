53d3b766564d15749d842382b93e728f3a7108b4
BTP-Magna <BTP_Magna@proton.me>
2025-04-30 08:42:18 +0700

    Make the function more modular

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -1,16 +1,23 @@
 function changeBodyColor() {
-    // Math.random() return value from 0 - 0.9999 never 1
+    // Get element body and write out new value to the style
+    document.body.style.backgroundColor = randomHue();
+}
+
+function randomHue() {
+    // Math.random() return value from 0 - 0.9999 never 1 so exclusive of 1
     // Math.floor() return rounds down of the input number
-    // Hue color will goes from 0 - 360
+
     // Math.floor(Math.random() * (max - min + 1) ) + min
     // will return between min and max inclusive on both end
+
+    // Hue color will goes from 0 - 360
     let hue = Math.floor(Math.random() * 360) + 1;
     // Saturation intensity of the color 50 - 100% 
     let sat = Math.floor(Math.random() * 50 + 1) + 50 + '%';
     // Lightness of dark or bright 0 - 100%
     let light = Math.floor(Math.random() * 100) + 1 + '%';
+    // alpha always set to 1
     let color = "hsla(" + hue + ', ' + sat + ', ' + light + ', 1)'; 
-    // Get element body and write out new value to the style
-    // alert(color);
-    document.body.style.backgroundColor = color;
+    // alert(color); // For debugging
+    return color;
 }
